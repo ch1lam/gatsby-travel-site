1abea3a888b8cb863fb3e36a26075bafefa6b104
chilam <chilam.Lee@outlook.com>
2022-03-27 23:51:35 +0800

    :sparkles: feat: add menu data

---
+++ src/data/MenuData.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description  :
+ * @Author       : ch1lam
+ * @Date         : 2022-03-27 23:39:18
+ * @LastEditTime : 2022-03-27 23:47:16
+ * @LastEditors  : chilam
+ * @FilePath     : \gatsby-travel-site\src\data\MenuData.ts
+ */
+export const menuData = [
+  { title: "About", link: "/about" },
+  { title: "Trips", link: "/trips" },
+  { title: "Careers", link: "/careers" },
+  { title: "Contact", link: "/contact" },
+];
