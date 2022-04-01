03a4efc76bf2bee882b6124c4e4ec32f07c16603
chilam <chilam.Lee@outlook.com>
2022-04-01 20:38:42 +0800

    :sparkles: feat: 添加 Footer 部分

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,11 +2,12 @@
  * @Description  :
  * @Author       : ch1lam
  * @Date         : 2022-03-27 22:36:05
- * @LastEditTime : 2022-03-27 23:32:57
+ * @LastEditTime : 2022-04-01 20:18:03
  * @LastEditors  : chilam
  * @FilePath     : \gatsby-travel-site\src\components\layout.tsx
  */
 import React from "react";
+import Footer from "./Footer";
 import Header from "./Header";
 import { GlobalStyle } from "./styles/GlobalStyles";
 
@@ -16,6 +17,7 @@ const Layout = ({ children }: any) => {
       <GlobalStyle />
       <Header />
       <main>{children}</main>
+      <Footer />
     </>
   );
 };
