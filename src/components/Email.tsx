116476822e508d85f5ee55c1ef6f3deead451403
chilam <chilam.Lee@outlook.com>
2022-04-08 22:50:24 +0800

    :sparkles: feat: 添加Email页滚动动画

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -2,23 +2,70 @@
  * @Description  :
  * @Author       : ch1lam
  * @Date         : 2022-04-01 18:46:54
- * @LastEditTime : 2022-04-05 17:39:20
+ * @LastEditTime : 2022-04-08 22:24:17
  * @LastEditors  : chilam
  * @FilePath     : \gatsby-travel-site\src\components\Email.tsx
  */
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import EmailBg from "../assets/images/email.jpg";
 import { Button } from "./Button";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Email = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.6,
+  });
+
+  const variants = {
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+    hidden: { opacity: 0, y: -100 },
+  };
+  const variants2 = {
+    visible: { opacity: 1, y: 0, transition: { duration: 0.6, delay: 0.4 } },
+    hidden: { opacity: 0, y: -100 },
+  };
+
+  const variants3 = {
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+    hidden: { opacity: 0, y: 100 },
+  };
+
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
   return (
     <EmailContainer>
       <EmailContent>
-        <h1>Get Access to Exclusive Offers</h1>
-        <p>Sign up for your newsletter below to get $100off your first tripi</p>
+        <motion.h1
+          ref={ref}
+          animate={controls}
+          initial="hidden"
+          variants={variants}
+        >
+          Get Access to Exclusive Offers
+        </motion.h1>
+        <motion.p
+          ref={ref}
+          animate={controls}
+          initial="hidden"
+          variants={variants2}
+        >
+          Sign up for your newsletter below to get $100off your first tripi
+        </motion.p>
         <form action="#">
-          <FormWrap>
+          <FormWrap
+            ref={ref}
+            animate={controls}
+            initial="hidden"
+            variants={variants3}
+          >
             <label htmlFor="email">
               <input type="email" placeholder="Enter your email" id="email" />
             </label>
@@ -93,7 +140,7 @@ const EmailContent = styled.div`
   }
 `;
 
-const FormWrap = styled.div`
+const FormWrap = styled(motion.div)`
   input {
     padding: 1rem 1.5rem;
     outline: none;
