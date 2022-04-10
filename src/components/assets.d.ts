b390390b8c7007a766e8fe27d438b24c7cf57625
chilam <chilam.Lee@outlook.com>
2022-04-10 20:37:20 +0800

    :bug: fix: 修复引入.mp4格式文件ts报错问题

---
+++ src/components/assets.d.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description  :
+ * @Author       : ch1lam
+ * @Date         : 2022-04-01 20:11:27
+ * @LastEditTime : 2022-04-10 20:33:06
+ * @LastEditors  : chilam
+ * @FilePath     : \gatsby-travel-site\src\components\assets.d.ts
+ */
+declare module "*.jpg";
+declare module "*.mp4";
