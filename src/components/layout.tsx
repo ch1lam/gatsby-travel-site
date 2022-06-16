661e7eda8f2c5b43e375e1398a92807287527107
chilam <chilam.Lee@outlook.com>
2022-06-16 23:47:17 +0800

    :label: style: 添加类型限定

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@
  * @Description  :
  * @Author       : ch1lam
  * @Date         : 2022-03-27 22:36:05
- * @LastEditTime : 2022-04-01 20:18:03
+ * @LastEditTime : 2022-05-07 16:48:07
  * @LastEditors  : chilam
  * @FilePath     : \gatsby-travel-site\src\components\layout.tsx
  */
@@ -11,12 +11,16 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { GlobalStyle } from "./styles/GlobalStyles";
 
-const Layout = ({ children }: any) => {
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
   return (
     <>
       <GlobalStyle />
       <Header />
-      <main>{children}</main>
+      {children}
       <Footer />
     </>
   );
